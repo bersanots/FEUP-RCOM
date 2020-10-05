fa4194f152dc8f7df64ea5511a137fcf0bf8fa98
Bernardo <bernardo.santos10@gmail.com>
2020-10-05 18:53:46 +0100

    Fixed errors on noncanonical.c, added printf messages

--- Aula 1/noncanonical.c
+++ Aula 1/noncanonical.c
@@ -15,7 +15,7 @@ volatile int STOP=FALSE;
 
 int main(int argc, char** argv)
 {
-    int fd,c, res, index = 0;
+    int fd,c, res;
     struct termios oldtio,newtio;
     char buf[255];
 
@@ -50,7 +50,7 @@ int main(int argc, char** argv)
     newtio.c_lflag = 0;
 
     newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
-    newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
+    newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
 
 
 
@@ -69,26 +69,25 @@ int main(int argc, char** argv)
     }
 
     printf("New termios structure set\n");
-	
-	
-	//while (STOP==FALSE) {       /* loop for input */
-    //  res = read(fd,buf,255);   /* returns after 5 chars have been input */
-    //  buf[res]=0;               /* so we can printf... */
-    /*  printf(":%s:%d\n", buf, res);
-      if (buf[0]=='\n') STOP=TRUE;
-    }*/
-	
-	while (STOP==FALSE) {       /* loop for input */
+
+    char message[255];
+    int index = 0;
+
+    while (STOP==FALSE) {       /* loop for input */
       res = read(fd,buf,1);   /* returns after 5 chars have been input */
       buf[res]=0;               /* so we can printf... */
       printf(":%s:%d\n", buf, res);
       message[index++] = buf[0];
       if (buf[0]=='\0') STOP=TRUE;
     }
-	
-	
-	res = write(fd,buf,cnt);
-	
+
+    printf("Message received: %s\n", message);
+    printf("%d bytes received\n", index);
+
+  
+    res = write(fd,message,index);
+    printf("Message resent: %s\n", message);
+    printf("%d bytes written\n", res);
 
 
 
