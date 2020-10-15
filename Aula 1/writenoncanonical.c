57164dbcdb17e3d08cd5d6cc4347c52c7b8b0235
BernardoFerreira00 <bernardoferreira2013@gmail.com>
2020-10-15 08:46:52 +0100

    correction

--- Aula 1/writenoncanonical.c
+++ Aula 1/writenoncanonical.c
@@ -148,6 +148,9 @@ int main(int argc, char** argv)
 		
 		if(index == 5) { //UA foi lido
 			correctUA = checkUA(message);
+			while(alarm(3) > 0) {
+				sleep(1);
+			}
 		}
 		
 		alarmFlag = FALSE;
