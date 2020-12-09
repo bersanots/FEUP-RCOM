6ecdd2cc0f44b07388c90098796e87a25f41d20d
BernardoFerreira00 <bernardoferreira2013@gmail.com>
2020-12-09 14:10:47 +0000

    creating and connecting to socket

--- parte 2/clientTCP.c
+++ parte 2/clientTCP.c
@@ -11,7 +11,7 @@
 #include <netdb.h>
 #include <strings.h>
 
-#define SERVER_PORT 6000
+#define SERVER_PORT 21
 #define SERVER_ADDR "192.168.28.96"
 
 struct UrlInfo {
@@ -21,6 +21,34 @@ struct UrlInfo {
 	char urlPath[255];
 };
 
+int createAndConnectToSocket(char* ip) {
+
+	int	sockfd;
+	struct	sockaddr_in server_addr;
+	char	buf[] = "Mensagem de teste na travessia da pilha TCP/IP\n";  
+	int	bytes;
+
+	/*server address handling*/
+	bzero((char*)&server_addr,sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = inet_addr(ip);	/*32 bit Internet address network byte ordered*/
+	server_addr.sin_port = htons(SERVER_PORT);		/*server TCP port must be network byte ordered */
+    
+	/*open an TCP socket*/
+	
+	if ((sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0) {
+    	perror("socket()");
+        exit(0);
+    }
+		
+	/*connect to the server*/
+    
+	if(connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0){
+    	perror("connect()");
+		exit(0);
+	}
+	
+}
 
 int divideUrl(char url[255], int size, struct UrlInfo* urlInfo) {
 
@@ -84,48 +112,38 @@ int divideUrl(char url[255], int size, struct UrlInfo* urlInfo) {
 
 int main(int argc, char** argv){
 
-	int	sockfd;
-	struct	sockaddr_in server_addr;
-	char	buf[] = "Mensagem de teste na travessia da pilha TCP/IP\n";  
-	int	bytes;
+	
 	struct UrlInfo urlInfo;
-
+	int	sockfd, bytes;
+	char buf[255];
+	struct hostent* h;
 
 	divideUrl(argv[1], strlen(argv[1]), &urlInfo);
 
-	printf("%s\n", urlInfo.host);
-	printf("%s\n", urlInfo.password);
-	printf("%s\n", urlInfo.urlPath);
-	printf("%s\n", urlInfo.user);
+	printf("1%s\n", urlInfo.host);
+	printf("2%s\n", urlInfo.password);
+	printf("3%s\n", urlInfo.urlPath);
+	printf("4%s\n", urlInfo.user);
 
+	if ((h=gethostbyname(urlInfo.host)) == NULL) {  
+        herror("gethostbyname");
+        exit(1);
+    }
+
+	printf("Host name  : %s\n", h->h_name);
+    printf("IP Address : %s\n",inet_ntoa(*((struct in_addr *)h->h_addr)));
 	
-	/*server address handling*/
-	//bzero((char*)&server_addr,sizeof(server_addr));
-	//server_addr.sin_family = AF_INET;
-	//server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);	/*32 bit Internet address network byte ordered*/
-	//server_addr.sin_port = htons(SERVER_PORT);		/*server TCP port must be network byte ordered */
-    
-	/*open an TCP socket*/
-	/*
-	if ((sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0) {
-    		perror("socket()");
-        	exit(0);
-    	}
-		*/
-	/*connect to the server*/
-    	/*
-		if(connect(sockfd, 
-	           (struct sockaddr *)&server_addr, 
-		   sizeof(server_addr)) < 0){
-        	perror("connect()");
-		exit(0);
+	if((sockfd = createAndConnectToSocket(inet_ntoa(*((struct in_addr *)h->h_addr)))) == -1) {
+		herror("error when creating socket or connecting");
+		exit(1);
 	}
-	*/
+	
     	/*send a string to the server*/
-	//bytes = write(sockfd, buf, strlen(buf));
-	//printf("Bytes escritos %d\n", bytes);
-
-	//close(sockfd);
+	/*
+	bytes = write(sockfd, buf, strlen(buf));
+	printf("Bytes escritos %d\n", bytes);
+	*/
+	close(sockfd);
 	exit(0);
 }
 
